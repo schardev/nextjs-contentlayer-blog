3dac46c65272c751e49dc7b862cd2db3c7361cbb
Saurabh Charde <saurabhchardereal@gmail.com>
2023-08-01 21:59:53 +0530

    use images from unsplash

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ const nextConfig = {
         hostname: "images.unsplash.com",
         pathname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/scharde/**",
+      },
     ],
   },
   experimental: {
