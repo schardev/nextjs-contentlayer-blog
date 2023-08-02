cffc615565796e975bb506e05cfadd24c4f73193
Saurabh Charde <saurabhchardereal@gmail.com>
2023-08-02 19:19:12 +0530

    feat: fetch image sizes without fully downloading them
    this is way faster than fetching the image and extracting width/height data from
    the buffer, but with this approach it usually detects them on the first chunk!

--- components/image.tsx
+++ components/image.tsx
@@ -1,14 +1,34 @@
-import { default as NextImage, ImageProps } from "next/image";
 import sizeOf from "image-size";
+import { ISizeCalculationResult } from "image-size/dist/types/interface";
+import { ImageProps, default as NextImage } from "next/image";
 import { readFile } from "node:fs/promises";
+import { IncomingMessage } from "node:http";
+import https from "node:https";
 import path from "node:path";
 import "server-only";
+
+// https://github.com/image-size/image-size/issues/258
+// https://github.com/nickadamson/messonry/commit/1604311247f077718650435b4ca38ae87b41e55d
+const getStreamImageSize = async (stream: IncomingMessage) => {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+    try {
+      // stop requesting data after dimensions are known
+      return sizeOf(Buffer.concat(chunks));
+    } catch (error) {
+      // Not enough buffer to determine sizes yet
+    }
+  }
+
+  return sizeOf(Buffer.concat(chunks));
+};
 
 const Image = async ({
   src,
   quality = 100,
   ...restProps
-}: ImageProps & { src: string }) => {
+}: Omit<ImageProps, "src"> & { src: string }) => {
   if (!src) return null;
   const isExternalImage = src.startsWith("https");
   const isPublicImage = src.startsWith("/");
@@ -16,16 +36,21 @@ const Image = async ({
   let Img: typeof NextImage | string = "img";
 
   try {
-    let img: Buffer | null = null;
-    if (isPublicImage) img = await readFile(path.join("public", src));
-    if (isExternalImage) {
-      img = await fetch(src).then(async (res) =>
-        Buffer.from(await res.arrayBuffer())
-      );
+    let size: ISizeCalculationResult | null = null;
+
+    if (isPublicImage) {
+      const img = await readFile(path.join("public", src));
+      size = sizeOf(img);
     }
 
-    if (img) {
-      const { width, height } = sizeOf(img);
+    if (isExternalImage) {
+      https.get(src, async (stream) => {
+        size = await getStreamImageSize(stream);
+      });
+    }
+
+    if (size) {
+      const { width, height } = size;
       imgProps.width = width;
       imgProps.height = height;
       Img = NextImage;
